a2f810430cb9b7cf57e30a7fb918da9758848696
Your Name <you@example.com>
2025-03-17 09:35:10 +0530

    fix: update the local script

--- scripts/install_all_canisters.sh
+++ scripts/install_all_canisters.sh
@@ -1,8 +1,10 @@
 # set -euo pipefail
-dfx stop && dfx start --clean --background --host 127.0.0.1:4943
+dfx stop && \
+    dfx start --clean --background --host 127.0.0.1:4943 && \
+    dfx canister create estate_backend
 
 scripts/candid_generator.sh
-cp ./src/backend/can.did ../estate-fe/ssr/did/backend.did
+cp ./src/backend/can.did ../estate-fe-pr-review/ssr/did/backend.did
 
 dfx deploy
 
